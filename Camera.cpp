8269721ebeaaa0fb6016f8110e0bdea18ad5062a
Piza_610 <kan10tourmaline@gmail.com>
2024-06-05 16:53:38 +0900

    write a code of C++

---
+++ Camera.cpp
@@ -0,0 +1,25 @@
+#include "opencv2/opencv.hpp"
+
+int main(int argh, char* argv[])
+{
+    cv::VideoCapture cap(0);    //デバイスのオープン
+
+    if(!cap.isOpened())     //カメラデバイスが正常にオープンしたか確認
+        return -1;
+
+    cv::Mat frame;  //取得したフレーム
+    while (cap.read(frame))
+    {
+        //取得したフレーム画像に対し、クレースケール変換や2値化などの処理を書き込む
+
+        cv::imshow("win",frame);
+        const int key = cv::waitKey(1);
+        if(key == 'q')
+            break;
+        else if(key == 's')
+            cv::imwrite("img.jpg", frame);
+    }
+    cv::destroyAllWindows();
+
+    return 0;
+}
