c1a3169a9fa6e03c0fed2a855bd3850b95df2648
Piza_610 <kan10tourmaline@gmail.com>
2024-06-06 23:53:20 +0900

    edit the Motor's code

---
+++ Motor.cpp
@@ -0,0 +1,26 @@
+#include <cstdio>     // printf()
+#include <wiringPi.h> // wiringPiSetupGpio() pinMode() pwmSetMode() pwmSetRange() pwmSetClock() pwmWrite() delay()
+
+int main(){
+    int pin = 17;
+
+    printf("Raspberry Pi wiringPi test program\n");
+    wiringPiSeetupGpio();
+    pinMode(pin, PWM_OUTPUT);
+    pwmSetMode(PWM_MODE_MS);
+
+    pwmSetClock(192);
+    pwmSetRange(2000);
+
+    while (true)
+    {
+        pwmWrite(pin, 50);
+        delay(1000);
+        pwmWrite(pin,150);
+        delay(1000);
+        pwmWrite(pin, 250);
+        delay(1000);
+    }
+
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Using GPIO
+
+
+## Moter.cpp
+### Module
+- [pigpio](https://qiita.com/fujita06/items/ca265fd4aa8f9474d966)
