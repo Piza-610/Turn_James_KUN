7bdacd27abb05b0cdd97533af60b4aec1bdd1b7c
Piza_610 <kan10tourmaline@gmail.com>
2024-06-07 00:25:50 +0900

    add comment & change val to the Motor's code

--- Motor.cpp
+++ Motor.cpp
@@ -5,17 +5,17 @@ int main(){
     int pin = 19;
 
     printf("Raspberry Pi wiringPi test program\n");
-    wiringPiSetupGpio();
-    pinMode(pin, PWM_OUTPUT);
-    pwmSetMode(PWM_MODE_MS);
+    wiringPiSetupGpio();        //clear WiringPi
+    pinMode(pin, PWM_OUTPUT);   //(pin_num, mode) mode is output or input
+    pwmSetMode(PWM_MODE_MS);    //change MarkSpace
 
     pwmSetClock(192);
     pwmSetRange(2000);
 
     while (true)
     {
-        pwmWrite(pin, 50);
-        delay(1000);
+        pwmWrite(pin, 150);  //(pin_num, output_val)
+        delay(1000);        //sleep
         pwmWrite(pin,150);
         delay(1000);
         pwmWrite(pin, 250);
