1d316e51fe06063c88f5d2b7482a807a520d02bc
Piza_610 <kan10tourmaline@gmail.com>
2024-06-07 11:33:16 +0900

    test

--- Motor.cpp
+++ Motor.cpp
@@ -5,20 +5,18 @@ int main(){
     int pin = 19;
 
     printf("Raspberry Pi wiringPi test program\n");
-    wiringPiSetupGpio();        //clear WiringPi
-    pinMode(pin, PWM_OUTPUT);   //(pin_num, mode) mode is output or input
-    pwmSetMode(PWM_MODE_MS);    //change MarkSpace
+    wiringPiSetupGpio();        // clear WiringPi
+    pinMode(pin, PWM_OUTPUT);   // (pin_num, mode) mode is output or input
+    pwmSetMode(PWM_MODE_MS);    // change MarkSpace
 
     pwmSetClock(192);
     pwmSetRange(2000);
 
     while (true)
     {
-        pwmWrite(pin, 150);  //(pin_num, output_val)
-        delay(1000);        //sleep
-        pwmWrite(pin,150);
-        delay(1000);
-        pwmWrite(pin, 250);
+        pwmWrite(pin,150);  // (pin_num, output_val) 90°
+        delay(1000);        // sleep
+        pwmWrite(pin, 200); // 180°
         delay(1000);
     }
 
