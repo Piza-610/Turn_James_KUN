5bf4665856352cab53f542732516dd8657dbfe7a
Piza_610 <kan10tourmaline@gmail.com>
2024-06-15 17:12:18 +0900

    Camera Test3

--- Source/Camera.cpp
+++ Source/Camera.cpp
@@ -1,21 +1,66 @@
 #include <opencv2/opencv.hpp>
+#include <vector>
 
-int main() {
-  cv::Mat img;
-  // /dev/video0でVideoCaptureを宣言
-  cv::VideoCapture cap(0);
+using namespace cv;
+using namespace std;
 
-  while (true) {
-    // カメラの画像をimgに代入
-    cap.read(img);
+int main(void) {
 
-    // imgの表示
-    cv::imshow("VideoCapture", img);
+	//デバイスのオープン
+	VideoCapture cap(0);
 
-    // escで終了
-    unsigned char key = cv::waitKey(2);
-    if (key == '\x1b') break;
-  }
+	//カメラデバイスが正常にオープンしたか確認．
+	if (!cap.isOpened())
+	{
+		//読み込みに失敗したときの処理
+		return -1;
+	}
 
-  return 0;
+	//取得したフレーム
+	Mat frame;
+
+	//カスケード分類器格納場所
+	CascadeClassifier cascade; 
+
+	//正面顔情報が入っているカスケード
+	cascade.load("C:/opencv/sources/data/haarcascades/haarcascade_frontalface_alt.xml"); 
+	
+	//輪郭情報を格納場所
+	vector<Rect> faces; 
+
+
+	//無限ループ
+	while (1)
+	{
+		//USBカメラが得た動画の１フレームを格納
+		cap >> frame;
+
+		//格納されたフレームに対してカスケードファイルに基づいて顔を検知
+		cascade.detectMultiScale(frame, faces, 1.1, 3, 0, Size(20, 20));
+
+		//検出した顔の個数"faces.size()"分ループを行う
+		for (int i = 0; i < faces.size(); i++) 
+		{
+			//検出した顔を赤色矩形で囲む
+			rectangle(frame, Point(faces[i].x, faces[i].y), Point(faces[i].x + faces[i].width, faces[i].y + faces[i].height), Scalar(0, 0, 255), 3); 
+		}
+
+
+		//画像を表示．
+		imshow("win", frame);
+
+		//キーボード入力を受け付ける	
+		const int key = waitKey(1);
+
+		//qボタンが押されたとき
+		if (key == 'q'/*113*/)
+		{
+			break;//whileループから抜ける．
+		}
+
+	}
+	//すべてのウィンドウを閉じる
+	destroyAllWindows();
+
+	return 0;
 }
